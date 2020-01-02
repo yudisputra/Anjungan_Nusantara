d8c72f126d18c4ca798bff98e5b93b1c3326a8e0
reflyiham <darkside988@gmail.com>
2020-01-02 14:07:01 +0700

    Revisi Controller

--- baru/Anjungan_Nusantara/application/controllers/Welcome.php
+++ baru/Anjungan_Nusantara/application/controllers/Welcome.php
@@ -6,7 +6,7 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		$data = [
-			'sidebar' => 'homepage/header',
+			'header' => 'homepage/header',
 			'content' => 'homepage/home',
 			'footer' => 'homepage/footer',
 		];
